9abc527400f331152753c50c4b9d23d1276770d1
DESKTOP-0A3K9BM\DELL <lethannhdat07@gmail.com>
2026-06-20 21:40:00 +0700

    add upload image functionality system

--- main.py
+++ main.py
@@ -4,59 +4,93 @@ from pydantic import BaseModel
 import base64
 
 load_dotenv()
+
 client = OpenAI()
+
+
 class DataFoodItem(BaseModel):
     name_food: str
     price: float
     quantity: int
 
+
 class Invoice(BaseModel):
     items: list[DataFoodItem]
-def Infornation_Invoice(text, image):
-    
+
+
+def Information_Invoice(text=None, image=None):
+
+    chat_histories = []
+
+    # upload ảnh
     if image is not None:
 
-        with open(image,"rb") as file_invoice:
+        with open(image, "rb") as file_invoice:
             img_base64 = base64.b64encode(
                 file_invoice.read()
             ).decode()
-    chat_histories = []
-    chat_histories.append(
-        {
-            'role':'user',
-            'content':
-            [
-                {
-                    'type':'input_text',
-                    'text':'Hãy đọc hóa đơn'
-                },
-                {
-                        'type':'input_image',
-                        'image_url':f"data:image/jpeg;base64,{img_base64}"
-                }
-                
-            ]
 
-        }
-    )
+        chat_histories.append(
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "input_text",
+                        "text": "Hãy đọc hóa đơn này"
+                    },
+                    {
+                        "type": "input_image",
+                        "image_url": f"data:image/jpeg;base64,{img_base64}"
+                    }
+                ]
+            }
+        )
+
+    #  text
+    elif text:
+        chat_histories.append(
+            {
+                "role": "user",
+                "content": text
+            }
+        )
+
+    else:
+        raise ValueError(
+            "Vui lòng nhập hóa đơn hoặc tải ảnh hóa đơn."
+        )
+
     response = client.responses.parse(
-        instructions="""Bạn là một chuyên gia đọc hóa đơn món ăn
+        instructions="""
+        Bạn là một chuyên gia đọc hóa đơn món ăn.
+
         Hãy trích xuất:
-        -Tên món ăn
-        -Giá món ăn
-        -Số lượng
-        Trả đúng schema JSON.""",
-        model= 'gpt-5.4-mini',
-        input=chat_histories, 
+        - Tên món ăn
+        - Giá món ăn
+        - Số lượng
+
+        Trả đúng schema JSON.
+        """,
+        model="gpt-5-mini",
+        input=chat_histories,
         temperature=0.1,
         text_format=Invoice
     )
 
-invoice = Infornation_Invoice(text,image)
+    return response.output_parsed
+
+invoice = Information_Invoice(
+    text="""
+    Phở bò 45000 x 2
+    Trà đá 5000 x 3
+    """
+)
+
+print(invoice)
 
 tong_hd = sum(
     item.price * item.quantity
     for item in invoice.items
 )
 
-print(f"Tổng tiền: {tong_hd}.VND")
+print(f"Tổng tiền: {tong_hd:,.0f} VND")
